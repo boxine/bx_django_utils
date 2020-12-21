dbcb6e86b5fb3e54d4a09c103ad483922704c4b3
Jens Diemer <jens.diemer@boxine.de>
2020-12-21 09:05:31 +0100

    Add humanize time stuff
    Use the new `human_duration()` in `TimetrackingBaseModel`, too. So it's easy to display human
    create/update time stamps in admin.
    
    Will be used in e.g.: django-huey-monitor ;)

--- bx_py_utils_tests/test_app/admin.py
+++ bx_py_utils_tests/test_app/admin.py
@@ -5,4 +5,6 @@ from bx_py_utils_tests.test_app.models import CreateOrUpdateTestModel
 
 @admin.register(CreateOrUpdateTestModel)
 class CreateOrUpdateTestModelAdmin(admin.ModelAdmin):
+    list_display = ('human_create_dt', 'human_update_dt', 'name', 'slug')
+    list_display_links = ('name', 'slug')
     readonly_fields = ('create_dt', 'update_dt')
---
+++ bx_py_utils_tests/tests/test_templatetags_humanize_time.py
@@ -0,0 +1,53 @@
+import datetime
+
+from django.test import SimpleTestCase
+from django.utils import translation
+
+from bx_py_utils.templatetags.humanize_time import human_duration
+from bx_py_utils.test_utils.datetime import parse_dt
+
+
+class HumanizeTimeTestCase(SimpleTestCase):
+    def test_basic(self):
+        with translation.override('en'):
+            result = human_duration(
+                parse_dt('2000-01-01T12:00:00+0000'),
+                parse_dt('2000-01-01T12:10:00+0000'),
+            )
+            assert result == '<span title="Jan. 1, 2000, noon">10.0\xa0minutes</span>'
+
+        with translation.override('en'):
+            result = human_duration(
+                parse_dt('2000-01-01T12:00:00+0000'),
+                parse_dt('2000-01-01T12:00:02+0000'),
+            )
+            assert result == '<span title="Jan. 1, 2000, noon">2.0\xa0seconds</span>'
+
+        with translation.override('de'):
+            result = human_duration(
+                parse_dt('2000-01-01T12:00:00+0000'),
+                parse_dt('2000-01-01T12:10:00+0000'),
+            )
+            assert result == '<span title="1. Januar 2000 12:00">10.0\xa0minutes</span>'
+
+        with translation.override('en'):
+            result = human_duration(
+                parse_dt('2000-01-01T12:32:12+0000'),
+                parse_dt('2000-01-01T12:20:10+0000'),
+            )
+            assert result == '<span title="Jan. 1, 2000, 12:32 p.m.">-12.0\xa0minutes</span>'
+
+        with translation.override('en'):
+            years_back = datetime.datetime.now() - datetime.timedelta(days=5 * 365)
+            result = human_duration(years_back)
+            assert result.endswith('>5.0\xa0years</span>')
+
+        assert human_duration(None) == ''
+        assert human_duration(value=object) == ''
+
+        with translation.override('en'):
+            result = human_duration(
+                datetime.date(2000, 1, 1),
+                datetime.date(2000, 6, 15),
+            )
+            assert result == '<span title="Jan. 1, 2000, midnight">5.5\xa0months</span>'
