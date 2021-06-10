67dbf6fd47ff1053dc16eb1bdc7d32b0e3d302b6
Jens Diemer <jens.diemer@boxine.de>
2021-06-10 09:34:52 +0200

    New: assert_html_response_snapshot()

--- bx_django_utils_tests/tests/test_html_assertion.py
+++ bx_django_utils_tests/tests/test_html_assertion.py
@@ -1,8 +1,14 @@
+from unittest import mock
+
+import django
+from bx_py_utils.compat import removesuffix
+from bx_py_utils.test_utils.snapshot import _get_caller_names
 from django.contrib import messages
 from django.contrib.messages.storage.session import SessionStorage
+from django.template.defaulttags import CsrfTokenNode
 from django.test import RequestFactory, SimpleTestCase
 
-from bx_django_utils.test_utils.html_assertion import HtmlAssertionMixin
+from bx_django_utils.test_utils.html_assertion import HtmlAssertionMixin, assert_html_response_snapshot
 
 
 class FakeResponse:
@@ -51,3 +57,31 @@ class HtmlAssertionTestCase(HtmlAssertionMixin, SimpleTestCase):
                 'Second X message.',
                 'The last message.'
             ])
+
+    def test_assert_html_response_snapshot(self):
+        # Add the Django version to snapshot name, because they have different outputs ;)
+        root_dir, snapshot_name = _get_caller_names()
+        snapshot_name = removesuffix(snapshot_name, '.snapshot')
+        version = django.__version__[:3]
+        snapshot_name += f'-django{version}'
+
+        with mock.patch.object(CsrfTokenNode, 'render', return_value='MockedCsrfTokenNode'):
+            assert_html_response_snapshot(
+                response=self.client.get(path='/admin/login/'),
+                status_code=200,
+                root_dir=root_dir,
+                snapshot_name=snapshot_name,
+            )
+
+        response = self.client.get(path='/admin/')
+        self.assertRedirects(
+            response, expected_url='/admin/login/?next=%2Fadmin%2F', fetch_redirect_response=False
+        )
+
+        # A redirect will be result in a empty snapshot file,
+        # but the status code is checked, too:
+        assert_html_response_snapshot(response, status_code=302)
+
+        msg = 'Status code is 302 but excepted 200'
+        with self.assertRaisesMessage(AssertionError, msg):
+            assert_html_response_snapshot(response)
