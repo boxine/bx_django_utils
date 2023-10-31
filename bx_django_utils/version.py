9f3e55538417b820b864fe2ddcd5076b2d3f5a57
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2023-10-31 17:02:33 +0100

    version command: Imply --raise-error when --write-file is given
    Nobody ever wants to write "unknown" as a version file – better to not write a version file at all and determine it on runtime.

--- bx_django_utils/version.py
+++ bx_django_utils/version.py
@@ -58,11 +58,17 @@ class DetermineVersionCommand(BaseCommand):
             help='Instead of determining the version from scratch, read it from file if possible',
         )
         parser.add_argument('-r', '--raise-error', action='store_true', help='Fail if the version is not set')
-        parser.add_argument('-w', '--write-file', action='store_true', help=f'Write to version file ({VERSION_FILE})')
+        parser.add_argument(
+            '-w',
+            '--write-file',
+            action='store_true',
+            help=f'Write to version file ({VERSION_FILE}). Implies --raise-error (we never want to write "unknown")',
+        )
 
     def handle(self, **options):
         func = get_version if options['get_from_file'] else determine_version
-        v = func(raise_error=options['raise_error'])
+        raise_error = options['raise_error'] and options['write_file']
+        v = func(raise_error=raise_error)
 
         if options['write_file']:
             VERSION_FILE.write_text(v)
