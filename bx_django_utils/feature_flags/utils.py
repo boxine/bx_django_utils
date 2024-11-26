56604e58a011ad8124469403cafc520762b6755e
flbraun <mail@chiron-online.de>
2024-11-26 11:41:14 +0100

    Add retval_factory parameter to @if_enabled
    Currently, if a feature flag is disabled, the @if_enabled decorator will always return `None`. This may break the caller if it expects a return value of the same type as it would receive when the flag is enabled, or make the caller code more verbose because of necessary `None` checks. Adding the new optional `retval_factory` parameter lets the caller specify which non-`None` value should be returned in that case.

--- bx_django_utils/feature_flags/utils.py
+++ bx_django_utils/feature_flags/utils.py
@@ -1,11 +1,11 @@
 import functools
-from typing import TYPE_CHECKING
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any
 
 from django.core.cache.backends.base import memcache_key_warnings
 from django.core.exceptions import ValidationError
 
 from bx_django_utils.feature_flags.exceptions import FeatureFlagDisabled
-
 
 if TYPE_CHECKING:
     from bx_django_utils.feature_flags.data_classes import FeatureFlag
@@ -17,20 +17,30 @@ def validate_cache_key(cache_key: str) -> None:
         raise ValidationError(messages)
 
 
-def if_feature(feature_flag: "FeatureFlag", raise_exception: bool = False):
+def if_feature(
+    feature_flag: "FeatureFlag",
+    raise_exception: bool = False,
+    retval_factory: Callable[[], Any] | None = None,
+):
     """
-    A decorator that only executed the decorated function if the given feature flag is enabled.
-    Performs a noop otherwise, or raises a FeatureFlagDisabled exception if raise_exception is True.
+    A decorator that only executes the decorated function if the given feature flag is enabled.
+
+    :param feature_flag: The feature flag to consider.
+    :param retval_factory: A factory that returns the value to be returned when the feature flag is disabled.
+    :param raise_exception: If True, raises a FeatureFlagDisabled exception if the feature flag is disabled.
     """
+    if raise_exception and retval_factory is not None:
+        raise ValueError('raise_exception=True and retval_factory are mutually exclusive')
 
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             if not feature_flag.is_enabled:
-                if not raise_exception:
-                    return
-                else:
+                if raise_exception:
                     raise FeatureFlagDisabled(feature_flag)
+                if retval_factory is not None:
+                    return retval_factory()
+                return
             return func(*args, **kwargs)
 
         return wrapper
